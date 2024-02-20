cd559a8a1ee81e17f9fabdeb212da6a1ed01bba6
p.tabakov <p.tabakov@geoscan.aero>
2024-02-20 16:05:01 +0300

    add navigation

--- mainApp/app/src/main/java/ru/etu/dylema/DilemmaActivity.kt
+++ mainApp/app/src/main/java/ru/etu/dylema/DilemmaActivity.kt
@@ -1,5 +1,6 @@
 package ru.etu.dylema
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -11,13 +12,25 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import kotlin.math.log
 
 @Composable
-fun DilemmaScreen(onClick: () -> Unit, img: Painter) {
+fun DilemmaScreen(navController: NavController) {
+    val taskProvider = remember {
+        mutableStateOf(TaskProvider())
+    }
+    val currentTask = remember {
+        mutableStateOf(taskProvider.value.current())
+    }
 
     Column(
         modifier = Modifier
@@ -35,7 +48,7 @@ fun DilemmaScreen(onClick: () -> Unit, img: Painter) {
             horizontalArrangement = Arrangement.Center
         ) {
             Image(
-                painter = img,
+                painter = painterResource(id = currentTask.value.imageId),
                 contentDescription = "Andy Rubin",
                 modifier = Modifier
                     .fillMaxSize()
@@ -50,13 +63,19 @@ fun DilemmaScreen(onClick: () -> Unit, img: Painter) {
             horizontalArrangement = Arrangement.SpaceAround
         ) {
             Column {
-                Button(onClick = { onClick() }) {
-                    Text(text = "Первое")
+                Button(onClick = {
+                    val task = taskProvider.value.prev()
+                    currentTask.value = task
+                }) {
+                    Text(text = "Назад")
                 }
             }
             Column {
-                Button(onClick = { onClick() }) {
-                    Text(text = "Второе")
+                Button(onClick = {
+                    val task = taskProvider.value.next()
+                    currentTask.value = task
+                }) {
+                    Text(text = "Вперед")
                 }
             }
         }
--- mainApp/app/src/main/java/ru/etu/dylema/MainActivity.kt
+++ mainApp/app/src/main/java/ru/etu/dylema/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.etu.dylema
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -15,8 +16,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -31,14 +34,10 @@ class MainActivity : ComponentActivity() {
             DylemaTheme {
                 NavHost(navController = navController, startDestination = "main_screen") {
                     composable("main_screen") {
-                        MainMenu {
-                            navController.navigate("dilemma_screen")
-                        }
+                        MainMenu(navController)
                     }
                     composable("dilemma_screen") {
-                        DilemmaScreen(onClick = {
-                                                navController.navigate("main_screen")
-                        }, img = painterResource(id = R.drawable.img))
+                        DilemmaScreen(navController)
                     }
                 }
 
@@ -46,6 +45,42 @@ class MainActivity : ComponentActivity() {
         }
 
     }
+}
+
+class TaskProvider() {
+
+    private val tasks = arrayListOf<Task>(
+        Task(R.drawable.img),
+        Task(R.drawable.ic_launcher_background),
+        Task(R.drawable.ic_launcher_foreground)
+    );
+
+    private var pointer = 0;
+
+    fun next(): Task {
+        if (pointer == (tasks.size - 1)) {
+            return tasks[pointer]
+        }
+
+        return tasks[++pointer]
+    }
+
+    fun prev(): Task {
+        if (pointer <= 0) {
+            return tasks[pointer]
+        }
+
+        return tasks[--pointer]
+    }
+
+    fun current(): Task {
+        return tasks[pointer]
+    }
+
+}
+
+class Task(var imageId: Int) {
+
 }
 
 @Composable
@@ -65,7 +100,7 @@ fun Greeting() {
 }
 
 @Composable
-fun MainMenu(onClick: () -> Unit) {
+fun MainMenu(navController: NavController) {
 
     Column(
         modifier = Modifier
@@ -74,7 +109,7 @@ fun MainMenu(onClick: () -> Unit) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Button(onClick = { onClick() }) {
+        Button(onClick = { navController.navigate("dilemma_screen") }) {
             Text(text = "Начать")
         }
     }

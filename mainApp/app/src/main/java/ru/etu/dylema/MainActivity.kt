b3fe4f4fcc407c33ba095697c7ded7565b4893f4
p.tabakov <p.tabakov@geoscan.aero>
2024-02-21 02:05:44 +0300

    сверстал главный экран

--- mainApp/app/src/main/java/ru/etu/dylema/MainActivity.kt
+++ mainApp/app/src/main/java/ru/etu/dylema/MainActivity.kt
@@ -3,19 +3,36 @@ package ru.etu.dylema
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -79,30 +96,181 @@ fun Greeting() {
 @Composable
 fun MainMenu(navController: NavController) {
 
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Box(
+        Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomStart
     ) {
-        Row {
-            Button(onClick = { navController.navigate("dilemma_screen") }) {
-                Text(text = "Начать")
+
+        Column(
+            modifier = Modifier
+                .background(Color(0xFFFFEBE3))
+                .fillMaxSize()
+                .padding(12.dp)
+                .background(Color(0xFFFFEBE3))
+                .border(2.dp, Color(0xFFCCCCCC))
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(3f),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.column),
+                    "",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 80.dp),
+                    alignment = Alignment.Center
+                )
+                Text(
+                    text = "Дилемма",
+                    color = Color(0xFF707070),
+                    fontSize = 44.sp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 30.dp, bottom = 40.dp),
+                    fontFamily = FontFamily(Font(resId = R.font.ledger_regular)),
+                    textAlign = TextAlign.Center
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(3f),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(
+                    colors = ButtonDefaults.buttonColors(Color(0xFFFFEBE3)),
+                    onClick = { navController.navigate("dilemma_screen") },
+                    modifier = Modifier
+                        .border(1.dp, Color(0xFF707070))
+                        .size(width = 210.dp, height = 55.dp)
+
+                ) {
+                    Text(
+                        text = "Играть", color = Color(0xFF707070),
+                        fontSize = 26.sp,
+                        fontFamily = FontFamily(Font(resId = R.font.ledger_regular)),
+                        textAlign = TextAlign.Center
+                    )
+                }
+                Spacer(modifier = Modifier.height(30.dp))
+                Button(
+                    colors = ButtonDefaults.buttonColors(Color(0xFFFFEBE3)),
+                    onClick = { navController.navigate("total_result_screen") },
+                    modifier = Modifier
+                        .border(1.dp, Color(0xFF707070))
+                        .size(width = 210.dp, height = 55.dp)
+
+                ) {
+                    Text(
+                        text = "Результаты", color = Color(0xFF707070),
+                        fontSize = 26.sp,
+                        fontFamily = FontFamily(Font(resId = R.font.ledger_regular)),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+            Column(modifier = Modifier.weight(2f)) {
+
             }
         }
-        Row {
-            Button(onClick = { navController.navigate("total_result_screen") }) {
-                Text(text = "Результаты")
-            }
-        }
+        Image(
+            painter = painterResource(id = R.drawable.man_sculpture), contentDescription = "",
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    DylemaTheme {
-        Greeting()
+    Box(
+        Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomStart
+    ) {
+
+        Column(
+            modifier = Modifier
+                .background(Color(0xFFFFEBE3))
+                .fillMaxSize()
+                .padding(12.dp)
+                .background(Color(0xFFFFEBE3))
+                .border(2.dp, Color(0xFFCCCCCC))
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(3f),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.column),
+                    "",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 80.dp),
+                    alignment = Alignment.Center
+                )
+                Text(
+                    text = "Дилемма",
+                    color = Color(0xFF707070),
+                    fontSize = 44.sp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 30.dp, bottom = 40.dp),
+                    fontFamily = FontFamily(Font(resId = R.font.ledger_regular)),
+                    textAlign = TextAlign.Center
+                )
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(3f),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(
+                    colors = ButtonDefaults.buttonColors(Color(0xFFFFEBE3)),
+                    onClick = { },
+                    modifier = Modifier
+                        .border(1.dp, Color(0xFF707070))
+                        .size(width = 210.dp, height = 55.dp)
+
+                ) {
+                    Text(
+                        text = "Играть", color = Color(0xFF707070),
+                        fontSize = 26.sp,
+                        fontFamily = FontFamily(Font(resId = R.font.ledger_regular)),
+                        textAlign = TextAlign.Center
+                    )
+                }
+                Spacer(modifier = Modifier.height(30.dp))
+                Button(
+                    colors = ButtonDefaults.buttonColors(Color(0xFFFFEBE3)),
+                    onClick = { },
+                    modifier = Modifier
+                        .border(1.dp, Color(0xFF707070))
+                        .size(width = 210.dp, height = 55.dp)
+
+                ) {
+                    Text(
+                        text = "Результаты", color = Color(0xFF707070),
+                        fontSize = 26.sp,
+                        fontFamily = FontFamily(Font(resId = R.font.ledger_regular)),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+            Column(modifier = Modifier.weight(2f)) {
+
+            }
+        }
+        Image(
+            painter = painterResource(id = R.drawable.man_sculpture), contentDescription = "",
+        )
     }
 }
--- mainApp/app/src/main/java/ru/etu/dylema/ui/theme/Type.kt
+++ mainApp/app/src/main/java/ru/etu/dylema/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package ru.etu.dylema.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import ru.etu.dylema.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,6 +16,11 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = FontFamily(Font(R.font.ledger_regular)),
+        fontWeight = FontWeight.Medium,
+        fontSize = 50.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
